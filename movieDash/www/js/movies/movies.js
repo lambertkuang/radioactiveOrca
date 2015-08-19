0bff07a49b3824288fe6930fe195e6c3c9f81c2f
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-19 11:48:03 -0700

    (refactor) add mock data to movies.js

--- movieDash/www/js/movies/movies.js
+++ movieDash/www/js/movies/movies.js
@@ -2,9 +2,50 @@ angular.module('moviedash.movies', [])
 
 .controller('MoviesCtrl', ['$scope', '$location', 'MovieClient', 'selected', 'convert', '$modal', '$sce',
   function ($scope, $location, MovieClient, selected, convert, $modal, $sce) {
-  // Code
-  //STUB: Replace with dynamic data
+  // Bool: change depending on whether you want to use mock data
+  var developerMode = true;
 
+  // the mock data
+  var stub = [{
+    dateObjectShowTime: 1440009027415,
+    id: "tt1449283",
+    imdbLink: "http://www.imdb.com/title/tt1449283/",
+    movieName: "Winnie the Pooh",
+    poster: "http://image.tmdb.org/t/p/w300/vokAA5Sxqc5zqK612kbuUKCTDPH.jpg",
+    rating: "PG",
+    showTime: "11:30am",
+    synopsis: "WINNIE THE POOH",
+    theaterAddress: "845 Market Street, San Francisco, CA",
+    theaterName: "Century San Francisco Centre 9 and XD",
+    trailerLink: "https://www.youtube.com/watch?v=zCqspczvg8o",
+    transitTime: "6 mins"
+  },
+  {
+    dateObjectShowTime: 1440009027485,
+    id: "tt0076363",
+    imdbLink: "http://www.imdb.com/title/tt0076363/",
+    movieName: "The Many Adventures of Winnie the Pooh",
+    poster: "http://image.tmdb.org/t/p/w300/vokAA5Sxqc5zqK612kbuUKCTDPH.jpg",
+    rating: "PG",
+    showTime: "12:00pm",
+    synopsis: "Much adventure. Many Pooh. Wow.",
+    theaterAddress: "845 Market Street, San Francisco, CA",
+    theaterName: "Century San Francisco Centre 9 and XD",
+    trailerLink: "https://www.youtube.com/watch?v=U4w3lJis7t8",
+    transitTime: "6 mins"
+  },{
+    dateObjectShowTime: 1440009027500,
+    id: "tt0061199",
+    imdbLink: "http://www.imdb.com/title/tt0061199/",
+    movieName: "Winnie the Pooh and the Honey Tree",
+    poster: "http://image.tmdb.org/t/p/w300/vokAA5Sxqc5zqK612kbuUKCTDPH.jpg",
+    rating: "PG",
+    showTime: "1:30pm",
+    synopsis: "This is the one with the honey tree and the umbrella and 'tut tut it looks like rain'!",
+    theaterName: "Century San Francisco Centre 9 and XD",
+    trailerLink: "https://www.youtube.com/watch?v=NitBpJaom5k",
+    transitTime: "6 mins"
+  }];
 
   $scope.selectMovie = function(movie) {
     selected.setStorage('movie', movie);
@@ -13,7 +54,13 @@ angular.module('moviedash.movies', [])
   $scope.localShowtime = function(showtime) {
     return convert.convertShowTime(showtime, selected.getStorage('leavingTime'));
   };
-  $scope.movies = MovieClient.getResults().data;
+
+  console.log(stub);
+
+  // loads the mock data or the server query, depending on if one 
+  // is in developerMode or not.
+  $scope.movies = developerMode ? stub : MovieClient.getResults().data;
+
 
   $scope.showTrailer = function(movie) {
     var link = movie.trailerLink;
@@ -52,5 +99,6 @@ angular.module('moviedash.movies', [])
     var finalRow = size % 3 || 3;
     return size - index <= finalRow;
   };
+
 }]);
 
